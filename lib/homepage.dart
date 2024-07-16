5d444b91fa2282f0acf8e065ea0ca7f3c275077b
AlanGitau <lewisgitau99@gmail.com>
2024-07-16 23:42:56 +0300

    kicks

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -20,7 +20,51 @@ class _HomepageState extends State<Homepage> {
       appBar:AppBar(
         backgroundColor:Colors.blueGrey,
         title: const Text('kicks soles'),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(icon: const Icon(Icons.menu), 
+              onPressed: () {  
+                Scaffold.of(context).openDrawer();
+              },);
+          }
+        ),
         ),  
+        drawer: Drawer(
+          backgroundColor: Colors.grey.shade900,
+          child: Column(
+            children: [
+              DrawerHeader(child: Image.network('https://pngimg.com/uploads/nike/nike_PNG11.png',
+              color: Colors.pink.shade50,)
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Icon(Icons.home, color: Colors.white),
+                  title: Text('Home', style: TextStyle(color: Colors.white),),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Icon(Icons.settings, color: Colors.white),
+                  title: Text('settings', style: TextStyle(color: Colors.white),),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Icon(Icons.logout_rounded, color: Colors.white),
+                  title: Text('Logout', style: TextStyle(color: Colors.white),),
+                ),
+              ),
+            ],),
+          ),
       body: Column(
         children: [
          
