5daef0ff94751e43e2fada27cf430ffe41a3653f
AlanGitau <lewisgitau99@gmail.com>
2024-07-17 01:05:46 +0300

    kicks

--- lib/components/bottomnavbar.dart
+++ lib/components/bottomnavbar.dart
@@ -1,35 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
-class Bottomnavbar extends StatelessWidget {
-  const Bottomnavbar({super.key});
+class BottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onTabChange;
+
+  const BottomNavBar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onTabChange,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const GNav(
-        padding: EdgeInsets.all(7),
-        gap:8,
-        backgroundColor: Color.fromARGB(255, 223, 213, 213),
-        tabBackgroundColor: Colors.grey,
-        tabs: [
-          GButton(
-            icon: Icons.home,
-            text: 'Home',
-            ),
-          GButton(
-            icon: Icons.favorite,
-            text: 'Favourites',
-            ),
-          GButton(
-            icon: Icons.shopping_cart,
-            text: 'cart',
-            ),
-          GButton(
-            icon: Icons.account_circle,
-            text: 'Profile',
-            ),
-        ]),
+      color: Color.fromARGB(255, 223, 213, 213),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+        child: GNav(
+          padding: EdgeInsets.all(7),
+          gap: 8,
+          backgroundColor: Color.fromARGB(255, 223, 213, 213),
+          tabBackgroundColor: Colors.grey,
+          selectedIndex: selectedIndex,
+          onTabChange: onTabChange,
+          tabs: [
+            GButton(icon: Icons.home, text: 'Home'),
+            GButton(icon: Icons.favorite, text: 'Favourites'),
+            GButton(icon: Icons.shopping_cart, text: 'Cart'),
+            GButton(icon: Icons.account_circle, text: 'Profile'),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-
 import 'components/bottomnavbar.dart';
+import 'shop.dart';
+import 'favorites.dart';
+import 'cart.dart';
+import 'profile.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -13,97 +13,88 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  int _selectedIndex = 0;
+
+  void navigatePage(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    Shop(),
+    Favorites(),
+    Cart(),
+    Profile(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.grey.shade300,
-      appBar:AppBar(
-        backgroundColor:Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
         title: const Text('kicks soles'),
         leading: Builder(
           builder: (context) {
-            return IconButton(icon: const Icon(Icons.menu), 
-              onPressed: () {  
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
                 Scaffold.of(context).openDrawer();
-              },);
-          }
+              },
+            );
+          },
         ),
-        ),  
-        drawer: Drawer(
-          backgroundColor: Colors.grey.shade900,
-          child: Column(
-            children: [
-              DrawerHeader(child: Image.network('https://pngimg.com/uploads/nike/nike_PNG11.png',
-              color: Colors.pink.shade50,)
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey.shade900,
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Image.network(
+                'https://pngimg.com/uploads/nike/nike_PNG11.png',
+                color: Colors.pink.shade50,
               ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Divider(
-                  color: Colors.white,
-                ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Divider(color: Colors.white),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: Icon(Icons.home, color: Colors.white),
+                title: Text('Home', style: TextStyle(color: Colors.white)),
               ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: ListTile(
-                  leading: Icon(Icons.home, color: Colors.white),
-                  title: Text('Home', style: TextStyle(color: Colors.white),),
-                ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: Icon(Icons.settings, color: Colors.white),
+                title: Text('settings', style: TextStyle(color: Colors.white)),
               ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: ListTile(
-                  leading: Icon(Icons.settings, color: Colors.white),
-                  title: Text('settings', style: TextStyle(color: Colors.white),),
-                ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: Icon(Icons.logout_rounded, color: Colors.white),
+                title: Text('Logout', style: TextStyle(color: Colors.white)),
               ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: ListTile(
-                  leading: Icon(Icons.logout_rounded, color: Colors.white),
-                  title: Text('Logout', style: TextStyle(color: Colors.white),),
-                ),
-              ),
-            ],),
-          ),
+            ),
+          ],
+        ),
+      ),
       body: Column(
         children: [
-         
-        const SizedBox(height: 10,),
-          Container(
-            //alignment:Alignment.center,
-            height: 60,width:double.infinity,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10)),
-            //color: Colors.white,
-           child: Text.rich(
-    TextSpan(
-      children: [
-        TextSpan(
-          text: 'Hello lets find some fresh kicks for you ',
-          style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
-        ),
-        TextSpan(
-          text: 'Authentic Kicks: ',
-          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.blue),
-        ),
-        TextSpan(
-          text: 'Guaranteed Quality & Fast Shipping.',
-          style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
-        ),
-      ],
-    ),
-  ),
-            ),
+          Expanded(
+            child: _pages[_selectedIndex],
+          ),
         ],
       ),
-       
-        //bottomNavigationBar
-      bottomNavigationBar: const Bottomnavbar(),
-        
-      );
-      
+      bottomNavigationBar: BottomNavBar(
+        selectedIndex: _selectedIndex,
+        onTabChange: navigatePage,
+      ),
+    );
   }
 }
