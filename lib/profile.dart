961c20808b3104a3f3654c4f381e9cdb7881cf90
AlanGitau <lewisgitau99@gmail.com>
2024-08-02 01:06:22 +0300

    kicks

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -11,10 +12,30 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('profile'),
-        ),
+    return  Scaffold(
+      body:Column(
+        children: [
+           Padding(
+             padding: const EdgeInsets.all(15.0),
+             child: ListTile(
+              contentPadding:EdgeInsets.only(right: 10),
+              tileColor: Colors.grey.shade200,
+              leading: Icon(Icons.person_2_outlined),
+              title: Text('Alan Gitau',
+              style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                ),
+              subtitle: Text('Lewisgitau99@gmail.com'),
+              trailing: IconButton(
+                icon:Icon(Icons.edit) ,
+                onPressed:(){
+             
+                },
+                ),
+             ),
+           ),
+           
+        ],
+      ),
     );
   }
 }
